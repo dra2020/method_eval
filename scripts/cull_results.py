a3096ab7e22d5e178d993677eec40936af4ba61d
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-06-02 16:32:13 -0700

    Added responsiveness metrics

--- scripts/cull_results.py
+++ scripts/cull_results.py
@@ -50,22 +50,34 @@ try:
     """
     Metrics (from dra-analytics):
 
-    * S_V = the estimated Democratic seats, using seat probabilities
-    * FPTP = the estimated number of Democratic seats using first past the post
-    * PR = the Democratic seats closest to proportional
+    * CD = the # of congressional districts
+    * Vf = the statewide D vote share
 
-    * BS_50 = Seat bias as a fraction of N
+    * S_V = the estimated D seats, using seat probabilities
+    * FPTP = the estimated # of D seats using first past the post
+    * PR = the D seats closest to proportional
+
+    * BS_50 = Seat bias as a fraction of CD
     * BV_50 = Votes bias as a fraction
     * DECL = Declination
     * GS = Global symmetry
 
     * EG = Efficiency gap as a fraction
-    * BS_V = Seats bias @ <V> (geometric)
+    * BS_V = Seats bias @ Vf (geometric)
     * PROP = Disproportionality
     * MM = Mean – median difference using statewide Vf
     * TO = Turnout bias
     * MM' = Mean – median difference using average district v 
     * LO = Lopsided outcomes
+
+    * BIG_R = Overall responsiveness or winner’s bonus 
+    * LIL_R = The point responsiveness at Vf (the slope of the S(V) curve at Vf)
+    * R_V = the estimated responsive districts, using seat probabilities
+
+    * L_Vf = the D vote share of the inferred S(V) point ~2.0% below Vf
+    * L_Sf = the corresponding D seat share
+    * U_Vf = the D vote share of the inferred S(V) point ~2.0% above Vf
+    * U_Sf = the corresponding D seat share
 
     """
 
@@ -85,6 +97,12 @@ try:
     tOf = s["bias"]["tOf"]
     mMd = s["bias"]["mMd"]
     lO = s["bias"]["lO"]
+
+    # Responsiveness
+
+    bigR = s["responsiveness"]["bigR"]
+    littleR = s["responsiveness"]["littleR"]
+    rD = s["responsiveness"]["rD"]
 
     # Add +/– 2% bracketing S(V) points
 
@@ -115,13 +133,13 @@ try:
             upper_Vf = pt["v"]
             upper_Sf = pt["s"]
 
-    # Append row
+    # Append row to file
 
     out_path = args.state + args.year + "-elections-analysis.csv"
     with open(out_path, "a") as f:
         if verbose:
             print(
-                "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(
+                "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(
                     "XX",
                     "YEAR",
                     "ELECTION",
@@ -141,6 +159,9 @@ try:
                     "TO",
                     "MM'",
                     "LO",
+                    "BIG_R",
+                    "LIL_R",
+                    "R_V",
                     "L_Vf",
                     "L_Sf",
                     "U_Vf",
@@ -149,7 +170,7 @@ try:
                 file=f,
             )
         print(
-            "{}, {}, {}, {}, {:.6f}, {:.6f}, {}, {}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}".format(
+            "{}, {}, {}, {}, {:.6f}, {:.6f}, {}, {}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}".format(
                 xx,
                 year,
                 election,
@@ -169,6 +190,9 @@ try:
                 tOf,
                 mMd,
                 lO,
+                bigR,
+                littleR,
+                rD,
                 lower_Vf,
                 lower_Sf,
                 upper_Vf,

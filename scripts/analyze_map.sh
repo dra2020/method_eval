ff6d91fb8a98b63ee8bf4be935409fe62ec2538d
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-06-03 07:22:11 -0700

    CA abstracts

--- scripts/analyze_map.sh
+++ scripts/analyze_map.sh
@@ -6,7 +6,7 @@
 # $ scripts/analyze_map.sh AR 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 #
 # $ scripts/analyze_map.sh AZ 2022 P2020 P2016 S2020 S2018 G2018 AG2018
-# $ scripts/analyze_map.sh CA 2022 P2020 P2016 S2020 S2018 G2018 AG2018
+# $ scripts/analyze_map.sh CA 2022 P2020 P2016 S20xx S20xx G2018 AG2018 <<< ???
 #
 # $ scripts/analyze_map.sh NC 2022 P2020 P2016 S2020 S2016 G2020 AG2020
 

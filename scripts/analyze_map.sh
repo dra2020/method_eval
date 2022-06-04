e78a8333847e66230c8de832dd241cb2a9fc265b
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-06-04 08:23:34 -0700

    ID data

--- scripts/analyze_map.sh
+++ scripts/analyze_map.sh
@@ -13,6 +13,7 @@
 # $ scripts/analyze_map.sh GA 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 # $ scripts/analyze_map.sh HI 2022 P2020 P2016 S2018 S2016 G2018        <<< No AG election!
 # $ scripts/analyze_map.sh IA 2022 P2020 P2016 S2020 S2016 G2018 AG2018
+# $ scripts/analyze_map.sh ID 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 #
 # $ scripts/analyze_map.sh NC 2022 P2020 P2016 S2020 S2016 G2020 AG2020 <<< DONE
 

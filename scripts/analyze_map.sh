21b6cec898d7c49c984580f44a63f48df63f806f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-06-05 06:12:37 -0700

    KS data

--- scripts/analyze_map.sh
+++ scripts/analyze_map.sh
@@ -15,6 +15,7 @@
 # $ scripts/analyze_map.sh ID 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 # $ scripts/analyze_map.sh IL 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 # $ scripts/analyze_map.sh IN 2022 P2020 P2016 S2018 S2016 G2020 AG2020
+# $ scripts/analyze_map.sh KS 2022 P2020 P2016 S2020 S2016 G2018 AG2018
 #
 # $ scripts/analyze_map.sh NC 2022 P2020 P2016 S2020 S2016 G2020 AG2020 <<< DONE
 

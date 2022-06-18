8d86ca64f404579a7c8c01e22ebf736df66c6ce3
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-06-18 06:57:59 -0700

    WIP

--- scripts/analyze_map.sh
+++ scripts/analyze_map.sh
@@ -109,6 +109,10 @@ cut -d "," -f 1,2,4,6,8,10,12,14 _$xx$yy-metrics-RAW.csv > _$xx$yy-metrics.csv
 cut -d "," -f 1,2,4,6,8,10,12,14 _$xx$yy-SV-points-RAW.csv > _$xx$yy-SV-points.csv
 cut -d "," -f 1,2,4,6,8,10,12,14 _$xx$yy-vi-points-RAW.csv > _$xx$yy-vi-points.csv
 
+../../scripts/calc_stats.py $xx $yy metrics str
+../../scripts/calc_stats.py $xx $yy SV-points float
+../../scripts/calc_stats.py $xx $yy vi-points int
+
 cd ../..
 
 echo

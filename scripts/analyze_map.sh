2bc5c512e34aa9735864086a85a77d9cf910b551
alecramsay <4155251+alecramsay@users.noreply.github.com>
2022-07-12 06:02:38 -0700

    IA not used

--- scripts/analyze_map.sh
+++ scripts/analyze_map.sh
@@ -38,7 +38,7 @@
 # scripts/analyze_map.sh SC 2022 P2020 P2016 S2020 S2016 G2018 AG2018 <<< DONE
 # scripts/analyze_map.sh TX 2022 P2020 P2016 S2020 S2018 G2018 AG2018 <<<
 # scripts/analyze_map.sh WA 2022 P2020 P2016 S2018 S2016 G2020 AG2020 <<< 
-# scripts/analyze_map.sh WI 2022 P2020 P2016 S2018 S2016 G2018 AG2018 
+# scripts/analyze_map.sh WI 2022 P2020 P2016 S2018 S2016 G2018 AG2018 <<<
 
 xx=$1
 yy=$2
